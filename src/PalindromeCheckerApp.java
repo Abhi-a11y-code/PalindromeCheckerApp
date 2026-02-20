7004a9d400b3f9d70a5f7f30cafb4e952fb72051
Abhi-a11y-code <abhimanyuverma295@gmail.com>
2026-02-20 09:20:12 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,11 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        showWelcomeMessage();
+    }
+
+    public static void showWelcomeMessage() {
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome App");
+        System.out.println("=================================");
+    }
 }
